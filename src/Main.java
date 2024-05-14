d8daad05bd8e051eb969c9288224a919db8036a9
Dmitry <dkonchik@gmail.com>
2024-05-14 20:02:14 +0300

    Hard Task removeByIndex

--- src/Main.java
+++ src/Main.java
@@ -3,51 +3,65 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-//        First task
-        Scanner scanner = new Scanner(System.in);
+////        First task
+//        Scanner scanner = new Scanner(System.in);
+//
+//        String line = scanner.nextLine();
+//        String[] numbers = line.split(",");
+//        Set<String> listOfNumber = new HashSet<>();
+//
+//        for (String s : numbers) {
+//            listOfNumber.add(s.replaceAll("\\s*", ""));
+//        }
+//        System.out.println(listOfNumber);
+//
+////        Second Task
+//        AnimalNameList animalNameList = new AnimalNameList();
+//
+//        animalNameList.addAnimalName("1");
+//        animalNameList.addAnimalName("2");
+//        animalNameList.addAnimalName("3");
+//        animalNameList.addAnimalName("4");
+//        animalNameList.addAnimalName("5");
+//        animalNameList.removeAnimalName();
+//        animalNameList.removeAnimalName();
+//        animalNameList.removeAnimalName();
+//        animalNameList.printAnimalName();
+//
+//
+////        Third task
+//
+//        Student student1 = new Student("Bob", "c2", 2, new int[]{3, 5, 6, 7});
+//        Student student2 = new Student("Bob1", "c3", 1, new int[]{3, 5, 6, 7});
+//        Student student3 = new Student("Bob2", "c3", 4, new int[]{1, 2, 1, 1});
+//        Student student4 = new Student("Bob3", "c4", 5, new int[]{3, 5, 4, 7});
+//        Student student5 = new Student("Bob4", "c4", 3, new int[]{3, 2, 2, 1});
+//        Student student6 = new Student("Bob5", "c6", 3, new int[]{1, 2, 2, 1});
+//
+//        University university = new University();
+//        university.addStudent(student1);
+//        university.addStudent(student2);
+//        university.addStudent(student3);
+//        university.addStudent(student4);
+//        university.addStudent(student5);
+//        university.addStudent(student6);
+//        university.printStudent();
+//        university.removeBedStudents();
+//        System.out.println();
+//        university.printStudent();
 
-        String line = scanner.nextLine();
-        String[] numbers = line.split(",");
-        Set<String> listOfNumber = new HashSet<>();
-
-        for (String s : numbers) {
-            listOfNumber.add(s.replaceAll("\\s*", ""));
-        }
-        System.out.println(listOfNumber);
-
-//        Second Task
-        AnimalNameList animalNameList = new AnimalNameList();
-
-        animalNameList.addAnimalName("1");
-        animalNameList.addAnimalName("2");
-        animalNameList.addAnimalName("3");
-        animalNameList.addAnimalName("4");
-        animalNameList.addAnimalName("5");
-        animalNameList.removeAnimalName();
-        animalNameList.removeAnimalName();
-        animalNameList.removeAnimalName();
-        animalNameList.printAnimalName();
-
-
-//        Third task
-
-        Student student1 = new Student("Bob", "c2", 2, new int[]{3, 5, 6, 7});
-        Student student2 = new Student("Bob1", "c3", 1, new int[]{3, 5, 6, 7});
-        Student student3 = new Student("Bob2", "c3", 4, new int[]{1, 2, 1, 1});
-        Student student4 = new Student("Bob3", "c4", 5, new int[]{3, 5, 4, 7});
-        Student student5 = new Student("Bob4", "c4", 3, new int[]{3, 2, 2, 1});
-        Student student6 = new Student("Bob5", "c6", 3, new int[]{1, 2, 2, 1});
-
-        University university = new University();
-        university.addStudent(student1);
-        university.addStudent(student2);
-        university.addStudent(student3);
-        university.addStudent(student4);
-        university.addStudent(student5);
-        university.addStudent(student6);
-        university.printStudent();
-        university.removeBedStudents();
-        System.out.println();
-        university.printStudent();
+        MyArrayList<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        list.removeByIndex(9);
+        System.out.println(list);
     }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -4,7 +4,7 @@ public class MyArrayList<T> {
 
     private T[] elements;
     private int size;
-    private final int DEFAULT_SIZE = 10;
+    private static final int DEFAULT_SIZE = 10;
 
     public MyArrayList() {
         elements = (T[]) new Object[DEFAULT_SIZE];
@@ -31,5 +31,25 @@ public class MyArrayList<T> {
 
     public int size() {
         return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyArrayList{" +
+                "elements=" + Arrays.toString(elements) +
+                '}';
+    }
+
+    public void removeByIndex(int index) {
+        if (index == 0) {
+            System.arraycopy(elements, 1, elements, 0, elements.length - 1);
+        } else if (index == elements.length - 1) {
+            elements[index] = null;
+        } else {
+            T[] newarr1 = (T[]) new Object[elements.length];
+            System.arraycopy(elements, 0, newarr1, 0, index);
+            System.arraycopy(elements, index + 1, newarr1, index, elements.length - index - 1);
+            elements = newarr1;
+        }
     }
 }
